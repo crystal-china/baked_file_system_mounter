9d963cda5e6c66d123d1dfb65f811119365ee2d4
Billy.Zheng <vil963@gmail.com>
2022-08-01 01:43:20 +0800

    Support mount multi-folder.

--- src/baked_file_system_mounter.cr
+++ src/baked_file_system_mounter.cr
@@ -1,24 +1,31 @@
 require "baked_file_system"
 
 module BakedFileSystemMounter
-  macro assemble(from, to)
+  macro assemble(mapping)
     {% begin %}
       {% root = system("pwd").strip.id %}
+      {% i, j = 0, 0 %}
 
       class BakedFileSystemStorage
         extend BakedFileSystem
-        BACKED_FILES = {{ run("./backed_files", from) }}
-
-        bake_folder "{{root}}/{{from.id}}"
+        {% for key, value in mapping %}
+          bake_folder "{{root}}/{{key.id}}"
+          @@backed_files_{{i}} = {{ run("./backed_files", key).strip }} of String
+          {% i += 1 %}
+        {% end %}
 
         def self.mount
-          BACKED_FILES.each do |filename|
-            target_file_name = filename.sub("{{from.id}}", "{{to.id}}")
+          {% for key, value in mapping %}
+            @@backed_files_{{j}}.each do |filename|
+              target_file_name = filename.sub("{{key.id}}", "{{value.id}}")
 
-            FileUtils.mkdir_p File.dirname(target_file_name) unless File.exists?(target_file_name)
+              FileUtils.mkdir_p File.dirname(target_file_name) unless File.exists?(target_file_name)
 
-            File.write(target_file_name, get(filename.sub("{{from.id}}/", "")).gets_to_end)
-          end
+              File.write(target_file_name, get(filename.sub("{{key.id}}/", "")).gets_to_end)
+
+              {% j += 1 %}
+            end
+          {% end %}
         end
       end
     {% end %}
