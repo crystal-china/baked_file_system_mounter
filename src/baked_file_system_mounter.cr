eae48420acf05e0624ef7d25c964175802e3c780
Billy.Zheng <vil963@gmail.com>
2022-07-31 19:49:53 +0800

    Refactor

--- src/baked_file_system_mounter.cr
+++ src/baked_file_system_mounter.cr
@@ -1,24 +1,24 @@
 require "baked_file_system"
 
 module BakedFileSystemMounter
-  macro mount(from, to)
+  macro assemble(from, to)
     {% begin %}
       {% root = system("pwd").strip.id %}
 
-      class FileStorage
+      class BakedFileSystemStorage
         extend BakedFileSystem
         BACKED_FILES = {{ run("./backed_files", from) }}
 
         bake_folder "{{root}}/{{from.id}}"
-      end
 
-      def mount_public_assets
-        FileStorage::BACKED_FILES.each do |filename|
-          target_file_name = filename.sub("{{from.id}}", "{{to.id}}")
+        def self.mount
+          BACKED_FILES.each do |filename|
+            target_file_name = filename.sub("{{from.id}}", "{{to.id}}")
 
-          FileUtils.mkdir_p File.dirname(target_file_name) unless File.exists?(target_file_name)
+            FileUtils.mkdir_p File.dirname(target_file_name) unless File.exists?(target_file_name)
 
-          File.write(target_file_name, FileStorage.get(filename.sub("{{from.id}}/", "")).gets_to_end)
+            File.write(target_file_name, FileStorage.get(filename.sub("{{from.id}}/", "")).gets_to_end)
+          end
         end
       end
     {% end %}
