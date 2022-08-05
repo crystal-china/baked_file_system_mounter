96be325e53aefca75374bba51b8b50f7edcb9130
Billy.Zheng <vil963@gmail.com>
2022-08-05 14:58:08 +0800

    Support Array as argument for mount to same name folder when run on runtime.

--- src/baked_file_system_mounter.cr
+++ src/baked_file_system_mounter.cr
@@ -2,19 +2,33 @@ require "baked_file_system"
 
 module BakedFileSystemMounter
   macro assemble(mapping)
+    {% unless mapping.is_a? HashLiteral || mapping.is_a? ArrayLiteral %}
+      {% raise "assemble only support Array or Hash as the only argument." %}
+    {% end %}
+
+    {% new_mapping = {} of String => String %}
+
+    {% if mapping.is_a? ArrayLiteral %}
+      {% for value in mapping %}
+        {% new_mapping[value] = value %}
+      {% end %}
+    {% elsif mapping.is_a? HashLiteral %}
+      {% new_mapping = mapping %}
+    {% end %}
+
     {% root = system("pwd").strip.id %}
     {% i, j = 0, 0 %}
 
     class BakedFileSystemStorage
       extend BakedFileSystem
-      {% for key, value in mapping %}
+      {% for key, value in new_mapping %}
         bake_folder "{{root}}/{{key.id}}"
         @@backed_files_{{i}} = {{ run("./backed_files", key).strip }} of String
         {% i += 1 %}
       {% end %}
 
       def self.mount
-        {% for key, value in mapping %}
+        {% for key, value in new_mapping %}
           @@backed_files_{{j}}.each do |filename|
             target_file_name = filename.sub("{{key.id}}", "{{value.id}}")
 
